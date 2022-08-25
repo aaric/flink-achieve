ec9be21ae5a0dc582358dc545098494bf96dd7c6
qlhan <qlhan@incarcloud.com>
2022-08-25 18:06:26 +0800

    0.1.0-SNAPSHOT data stream api

--- src/test/java/com/sample/flink/BoundedStreamTextTests.java
+++ src/test/java/com/sample/flink/BoundedStreamTextTests.java
@@ -1,6 +1,12 @@
 package com.sample.flink;
 
-import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,7 +19,19 @@ public class BoundedStreamTextTests {
 
     @Test
     public void testCount() throws Exception {
-        // DataSet API
-        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        // DataStream API
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> dss = env.readTextFile("input/sample.txt");
+        SingleOutputStreamOperator<Tuple2<String, Long>> operator = dss.flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                out.collect(Tuple2.of(word, 1L));
+            }
+        }).returns(Types.TUPLE(Types.STRING, Types.LONG));
+        KeyedStream<Tuple2<String, Long>, String> stream = operator.keyBy(data -> data.f0);
+        SingleOutputStreamOperator<Tuple2<String, Long>> sum = stream.sum(1);
+        sum.print();
+
+        env.execute();
     }
 }
