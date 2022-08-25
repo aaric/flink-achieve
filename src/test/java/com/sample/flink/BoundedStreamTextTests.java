b6845c1efc6f2d1f8851f06706cb060f9886f37c
qlhan <qlhan@incarcloud.com>
2022-08-25 17:44:41 +0800

    0.1.0-SNAPSHOT data stream api

---
+++ src/test/java/com/sample/flink/BoundedStreamTextTests.java
@@ -0,0 +1,19 @@
+package com.sample.flink;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.junit.jupiter.api.Test;
+
+/**
+ * BoundedStreamTextTests
+ *
+ * @author Aaric, created on 2022-08-24T14:18.
+ * @version 0.1.0-SNAPSHOT
+ */
+public class BoundedStreamTextTests {
+
+    @Test
+    public void testCount() throws Exception {
+        // DataSet API
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+    }
+}
